ca088d36ba12857e27f50c555133ec3ae9100335
Ajay-kharat-007 <kharatajay023@gmail.com>
2023-04-07 11:46:18 +0530

    first change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PrivacypolicyComponent } from './components/privacypolicy/privacypolicy.component';
+import { DisclaimerComponent } from './components/disclaimer/disclaimer.component';
+import { TermsAndConditionComponent } from './components/terms-and-condition/terms-and-condition.component';
+import { NopageComponent } from './components/nopage/nopage.component';
+import { ContentComponent } from './components/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'privacy', component: PrivacypolicyComponent },
+  { path: 'disclaimer', component: DisclaimerComponent },
+  { path: 'terms', component: TermsAndConditionComponent },
+  { path: 'content', component: ContentComponent },
+  { path: '**', component: NopageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PrivacypolicyComponent } from './components/privacypolicy/privacypolicy.component';
+import { DisclaimerComponent } from './components/disclaimer/disclaimer.component';
+import { TermsAndConditionComponent } from './components/terms-and-condition/terms-and-condition.component';
+import { NopageComponent } from './components/nopage/nopage.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PrivacypolicyComponent,
+    DisclaimerComponent,
+    TermsAndConditionComponent,
+    NopageComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
